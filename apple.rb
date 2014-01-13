25c462b3ae3461529ec9bb80af3c5b310dcb8a05
Logan Hasson <logan.hasson@gmail.com>
2014-01-13 11:33:51 -0500

    Add solution

---
+++ apple.rb
@@ -0,0 +1,11 @@
+fruit_array = ["apple", "orange", "apple"]
+
+# Write your code here.
+
+def apple_picker_with_select(fruit_array)
+  fruit_array.select { |item| item == "apple" }
+end
+
+def apple_picker_with_collect(fruit_array)
+  fruit_array.collect { |item| item if item == "apple" }.compact
+end
