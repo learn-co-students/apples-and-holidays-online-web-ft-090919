09d875ba267f60d72410b7fe5ee9966b9b1f421d
Katie Hoffman <ktahoffman@gmail.com>
2014-10-12 11:48:48 -0400

    fix calling class on a hash.class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/apple'
 require_relative '../lib/holiday'
 
+require 'pry'
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   #config.order = 'default'
