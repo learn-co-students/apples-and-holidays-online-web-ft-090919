b734c150e80903179f44cb8ff107d09df9a9256f
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-07-07 09:42:10 -0400

    updated readme

--- lib/apple.rb
+++ lib/apple.rb
@@ -1,10 +1,12 @@
 def apple_picker_with_select(fruit_list)
   # fruit_list will look something like: ["apple", "banana", "apple"]
   # you want to return an array that just has apples in it
+  
 end
 
 def apple_picker_with_collect(fruit_list)
   # fruit_list will look something like: ["apple", "banana", "apple"]
   # you want to return an array that just the apples, just like the 
   # challenge above
+  
 end
