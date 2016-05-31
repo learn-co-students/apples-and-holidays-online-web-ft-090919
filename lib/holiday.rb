e5664b179c682245d2d06542d5839414cdcf4271
alexgriff <alexalexgriffith@gmail.com>
2016-05-31 14:00:44 -0400

    Fix issue #40

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,29 +1,31 @@
-def second_supply_for_fourth_of_july(supplies_hash)
-  supplies_hash[:summer][:fourth_of_july][1]
+def second_supply_for_fourth_of_july(holiday_hash)
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
-def add_supply_to_winter_holidays(supplies_hash, supply)
-  supplies_hash[:winter].each do |holiday, supplies|
+def add_supply_to_winter_holidays(holiday_hash, supply)
+  holiday_hash[:winter].each do |holiday, supplies|
     supplies << supply
   end
 end
+ 
 
-def add_supply_to_memorial_day(supplies_hash, supply)
-  supplies_hash[:spring][:memorial_day] << supply
+def add_supply_to_memorial_day(holiday_hash, supply)
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
-def add_new_holiday_with_supplies(supplies_hash, season, new_holiday_name, supplies_list)
-  supplies_hash[season][new_holiday_name] = supplies_list
-  supplies_hash
+def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
+  
 end
 
-def all_winter_holiday_supplies(supplies_hash)
+def all_winter_holiday_supplies(holiday_hash)
   holiday_supplies[:winter].map do |holiday, supplies|
     supplies
   end.flatten
 end
 
-def all_supplies_in_holidays(supplies_hash)
+def all_supplies_in_holidays(holiday_hash)
   holiday_supplies.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
@@ -32,8 +34,8 @@ def all_supplies_in_holidays(supplies_hash)
   end
 end
 
-def all_holidays_with_bbq(supplies_hash)
-  supplies_hash.map do |season, holidays|
+def all_holidays_with_bbq(holiday_hash)
+  holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supplies|
       holiday if supplies.include?("BBQ")
     end
