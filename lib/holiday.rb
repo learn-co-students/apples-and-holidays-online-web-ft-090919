d19861973cb25852e9454c290eb53ab6931ee2fb
Jason Decker <curiositypaths@users.noreply.github.com>
2017-12-14 15:21:59 -0500

    Use parameter passed to method call vs variable set in the spec

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,13 +20,13 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_supplies[:winter].map do |holiday, supplies|
+  holiday_hash[:winter].map do |holiday, supplies|
     supplies
   end.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  holiday_supplies.each do |season, holidays|
+  holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
       puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
