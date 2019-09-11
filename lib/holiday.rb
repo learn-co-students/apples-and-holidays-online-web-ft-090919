940cf21f21dfbf661b8642933e2464aef68389ad
Kenneth Spatta <kenyounot@gmail.com>
2019-09-11 03:13:00 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,30 +18,41 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  
+  
+  holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_supplies[:winter].each do |idx, value| 
+    value << supply
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  
+  holiday_hash[:spring][:memorial_day] << supply
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  
+  holiday_hash[season][holiday_name] = supply_array
+  
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  
+   holiday_hash[:winter].values.flatten
 
 end
 
@@ -53,15 +64,34 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season,holiday_hash|
+    season_string = season.to_s.capitalize!
+    puts "#{season_string}:"
+    holiday_hash.each do |attribute, value|
+       attr_string = attribute.to_s.split('_').map(&:capitalize!).join(" ")
+       puts "  #{attr_string}: #{value.join(', ')}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+	holiday_names = []
+  holiday_hash.each do |season, hol_hash|
+    puts "#{season} #{hol_hash}"
+    hol_hash.each do |attr, value|
+			puts "#{attr} #{value}"
+				value.each do |idx|
+					if idx == "BBQ"
+						holiday_names << attr
+					end
 
+				end 
+		end
+  end
+	holiday_names
 end
-
 
 
 
