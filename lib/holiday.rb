6c7e3d0ee869b278e0efa0e215e31bffd97387e2
= <=>
2019-09-11 16:04:44 -0400

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,36 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
+  holiday_hash
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply
+  holiday_hash
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,13 +58,30 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, data|
+    puts season.to_s.capitalize + ":"
+    data.each do |holiday, supplies|
+      holiday = holiday.to_s.split("_").map { |word| word.capitalize }.join(" ")
+      supplies = supplies.each_with_index.reduce("") do |memo, (supply, index)|
+        unless index == supplies.length - 1
+          memo + supply.to_s + ", "
+        else
+          memo + supply.to_s
+        end
+      end
+      puts "  #{holiday}: #{supplies}"
+    end
+  end
 end
-
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.reduce([]) do |memo, (season, data)|
+    data.reduce(memo) do |memo, (holiday, supplies)|
+      memo << holiday if supplies.include?("BBQ")
+      memo
+    end
+  end
 end
 
 
